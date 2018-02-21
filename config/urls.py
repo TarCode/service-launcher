041e1a37150a7dff1d3cdabd414ac77bc3c66101
TarCode <tarcode33@gmail.com>
2018-02-21 09:13:08 +0200

    added views as templates

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 
+from django.views.generic import TemplateView
+
 from . import views
 
 import debug_toolbar
@@ -10,6 +12,7 @@ admin.autodiscover()
 
 urlpatterns = (
     # Dashboard
+		url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^dashboard/', include(admin.site.urls)),
     # Views
     url(r'^api/', include('launcher_service.urls', namespace='launcher_service')),
